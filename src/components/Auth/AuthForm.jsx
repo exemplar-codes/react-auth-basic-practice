ecc52ee7de259f9ceee8a6330cdbb38495561117
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-11-20 01:07:19 +0530

    Add signup flow

--- src/components/Auth/AuthForm.jsx
+++ src/components/Auth/AuthForm.jsx
@@ -1,12 +1,68 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 import classes from "./AuthForm.module.css";
 
+const firebaseAuthAPIKey = "AIzaSyBEu9KX4CD9qlDQZys5NddOUQJnmrfkr2Y";
+const signupAPIUrl = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${firebaseAuthAPIKey}`;
+const loginAPIUrl = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${firebaseAuthAPIKey}`;
+
+async function postAsJSON(url, body = {}) {
+  return fetch(url, {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const emailInputRef = useRef(),
+    passwordInputRef = useRef();
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    // maybe empty validation
+
+    (async () => {
+      setIsLoading(true);
+
+      let resp;
+      if (isLogin) {
+        resp = await postAsJSON(loginAPIUrl, {
+          email: enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true,
+        });
+      } else {
+        resp = await postAsJSON(signupAPIUrl, {
+          email: enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true,
+        });
+      }
+
+      const data = await resp.json();
+
+      if (resp.ok) {
+        console.log("SUCCESS", data);
+      } else {
+        alert(data?.error?.message ?? "Authentication failed");
+      }
+
+      setIsLoading(false);
+    })();
   };
 
   return (
@@ -15,14 +71,24 @@ const AuthForm = () => {
       <form>
         <div className={classes.control}>
           <label htmlFor="email">Your Email</label>
-          <input type="email" id="email" required />
+          <input type="email" id="email" required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="password">Your Password</label>
-          <input type="password" id="password" required />
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordInputRef}
+          />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? "Login" : "Create Account"}</button>
+          {!isLoading && (
+            <button onClick={submitHandler}>
+              {isLogin ? "Login" : "Create Account"}
+            </button>
+          )}
+          {isLoading && <p>Loading...</p>}
           <button
             type="button"
             className={classes.toggle}
