86dd1dc1ec0bd963c26ce4add0216561ad2acbec
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-11-27 14:21:36 +0530

    Use token expiry in app code

--- src/components/Auth/AuthForm.jsx
+++ src/components/Auth/AuthForm.jsx
@@ -60,7 +60,7 @@ const AuthForm = () => {
       const data = await resp.json();
 
       if (resp.ok) {
-        authCtx.login(data.idToken); // idToken identifier used by Firebase
+        authCtx.login(data.idToken, data.expiresIn); // idToken identifier used by Firebase
         history.replace("/"); // redirect to home for successful sign up, log in
       } else {
         alert(data?.error?.message ?? "Authentication failed");
