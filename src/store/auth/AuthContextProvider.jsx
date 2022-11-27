6918746e20fb27a455d5bef74061e0bd17098b17
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-11-27 13:48:42 +0530

    Implement persistent login + logout + on visit expiry check logout

--- src/store/auth/AuthContextProvider.jsx
+++ src/store/auth/AuthContextProvider.jsx
@@ -1,16 +1,48 @@
 import { useState } from "react";
 import AuthContext from "./AuthContext";
 
+// assuming duration is in seconds
+function calculateFutureDateTime(duration) {
+  return new Date(Date.now() + 1000 * duration);
+}
+
 function AuthContextProvider(props) {
-  const [token, setToken] = useState("");
+  const storedToken = localStorage.getItem("auth-token") ?? ""; // run once - login, logout or automatic token change
+
+  if (storedToken) {
+    // check if token has expired on visit
+    const storedExpiryDateTime = new Date(
+      localStorage.getItem("auth-token-expiry")
+    );
+    const currentDateTime = new Date();
+
+    if (storedExpiryDateTime < currentDateTime) logoutHandler();
+  }
+
+  const [token, setToken] = useState(storedToken);
 
   const resetToken = () => setToken("");
+
+  const loginHandler = (token, expiryDuration = 5) => {
+    localStorage.setItem("auth-token", token);
+    localStorage.setItem(
+      "auth-token-expiry",
+      calculateFutureDateTime(expiryDuration).toString()
+    );
+    setToken(token);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("auth-token");
+    localStorage.removeItem("auth-token-expiry");
+    resetToken();
+  };
 
   const authContextValue = {
     token: token,
     isLoggedIn: token !== "",
-    login: setToken,
-    logout: resetToken,
+    login: loginHandler,
+    logout: logoutHandler,
   };
 
   return (
